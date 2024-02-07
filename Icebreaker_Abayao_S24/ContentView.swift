45df3f8ad8a5de5da9fb267d7777a0d31b4b301a
ea <eabayschool>
2024-02-06 19:50:38 -0500

    in class progress before break

--- Icebreaker_Abayao_S24/ContentView.swift
+++ Icebreaker_Abayao_S24/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Icebreaker_Abayao_S24
+//  Icebreaker-Abayayo-S24
 //
 //  Created by ea on 2/6/24.
 //
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var textFirstName: String = ""
+    @State var textLastName: String = ""
+    @State var textPrefName: String = ""
+    @State var textAnswer: String = ""
+    @State var textQuestion: String = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Icebreaker")
+                .font(.system(size: 40))
+                .bold()
+            Text("Built with Swift UI")
+            
+            TextField("First Name", text: $textFirstName)
+            
+            TextField("Last Name", text: $textLastName)
+            
+            TextField("Preferred Name", text: $textPrefName)
+            
+            Button(action: {
+                setRandomQuestion()}){
+                Text("Get a new random question")
+                        .font(.system(size: 28))
+            }
+            Text(textQuestion)
+            
+            TextField("Answer", text: $textAnswer)
+            
+            Button(action: {
+                writeStudentToFirebase()}){
+                Text("Submit")
+                        .font(.system(size: 28))
+            }
         }
+        .font(.largeTitle)
+        .multilineTextAlignment(.center)
+        .autocorrectionDisabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
         .padding()
+    }
+    
+    func setRandomQuestion(){
+        print("Set random question pressed")
+    }
+    
+    func writeStudentToFirebase(){
+        print("Submit button pressed")
+        print("First Name: \(textFirstName)")
+        print("Last Name: \(textLastName)")
+        print("Pref Name: \(textPrefName)")
+        print("Answer: \(textAnswer)")
     }
 }
 
