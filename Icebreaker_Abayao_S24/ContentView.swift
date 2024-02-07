9f4d1e39a4ef098e39270151a4f308fe86831052
ea <eabayschool>
2024-02-06 19:48:49 -0500

    Initial Commit

---
+++ Icebreaker_Abayao_S24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Icebreaker_Abayao_S24
+//
+//  Created by ea on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Icebreaker_Abayao_S24/Icebreaker_Abayao_S24App.swift
@@ -0,0 +1,17 @@
+//
+//  Icebreaker_Abayao_S24App.swift
+//  Icebreaker_Abayao_S24
+//
+//  Created by ea on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Icebreaker_Abayao_S24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
